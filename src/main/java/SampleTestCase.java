07f75e7cd4bc76c2471aa5bb28de138832de8b4c
mkhabenko <31030147+mkhabenko@users.noreply.github.com>
2017-10-12 18:10:15 +0300

    Initial commit

---
+++ src/main/java/SampleTestCase.java
@@ -0,0 +1,35 @@
+import controllers.UserController;
+import models.User;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.web.client.RestTemplate;
+
+import static org.junit.Assert.*;
+
+public class SampleTestCase {
+
+    private UserController userController;
+
+    @Before
+    public void setUp() {
+        userController = new UserController();
+    }
+
+    @Test
+    public void testUserInfoResponse() {
+          RestTemplate restTemplate = new RestTemplate();
+          restTemplate.getForObject("http://marketing-web/Tavriav790_T/ExternalProjects/ExternalAPI.WebApp/EditHouseHoldRole")
+
+
+    //    User remoteUser = userController.getUserByUsername("maxkolotilkin");
+  //      assertTrue(remoteUser.getHtml_url().contains("https://github.com/maxkolotilkin"));
+    }
+
+  //  @Test
+    public void testFollowers() {
+        User[] followers = userController.getFollowersByUser("Khabenko");
+        assertTrue(followers.length == 0 || followers == null );
+    }
+
+
+}
---
+++ src/main/java/controllers/Client.java
@@ -0,0 +1,8 @@
+package controllers;
+
+public class Client {
+
+    String MobilePhone;
+   int BonusRoleType;
+   int CouponRoleType;
+}
